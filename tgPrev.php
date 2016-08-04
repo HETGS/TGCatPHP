9c7ec8a03a17ecf8a0de4370e84cef28a89083b1
hamogu <hgunther@mit.edu>
2016-08-04 16:02:40 -0400

    Add GPL 2 license to js and php files

--- tgPrev.php
+++ tgPrev.php
@@ -19,6 +19,29 @@ ob_start( "ob_gzhandler" );
 ##            Emma Reishus
 ##
 ##############################################################################
+
+/*
+Copyright (C) 2010 Massachusetts Institute of Technology 
+
+This software was developed by the MIT Kavli Institute for
+Astrophysics and Space Research under contract SV3-73016 from the
+Smithsonian Institution.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA
+*/
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
